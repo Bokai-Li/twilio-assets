2a088efe52d06084d530c132c435939d6e93b868
Bokai Li <bokai247@gmail.com>
2021-07-14 20:53:29 -0400

    Initial Commit

---
+++ twilioDemo/twilioDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  twilioDemoApp.swift
+//  twilioDemo
+//
+//  Created by Bokai Li on 7/14/21.
+//
+
+import SwiftUI
+
+@main
+struct twilioDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
